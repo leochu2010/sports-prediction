8d1de81f28c999fad0843d4cfe5cd9043450ae06
leochu <leochu@leochu-laptop>
2017-02-04 01:46:33 +0800

    scrap whoscored match detail

--- data/whoscored/scrapy/sports/sports/spiders/whoscored.py
+++ data/whoscored/scrapy/sports/sports/spiders/whoscored.py
@@ -4,6 +4,7 @@ import re
 import time
 from selenium import webdriver
 from scrapy_splash import SplashRequest
+from itertools import chain
 
 
 #run with command: scrapy crawl whoscored
@@ -14,11 +15,13 @@ class WhoscoredSpider(scrapy.Spider):
     allowed_domains = ["www.whoscored.com"]
         
     START_URL="https://www.whoscored.com/Matches/1085146/Live"
+        
     DOMAIN = 'www.whoscored.com'
 
     def start_requests(self):
         urls = [
-            self.START_URL,            
+            self.START_URL,
+            "https://www.whoscored.com/Matches/1085239",            
         ]
         for url in urls:
             #yield scrapy.Request(url=url, callback=self.parse)
@@ -30,36 +33,53 @@ class WhoscoredSpider(scrapy.Spider):
     def parse(self, response):
 
         match_report_link = 'https://'+self.DOMAIN+response.css('div.layout-content-2col-left li:nth-of-type(5) a::attr(href)').extract_first()        
-                        
-        yield {
+        
+        match = {
             'home_team_name': response.css("td.team:nth-of-type(1) a.team-link::text").extract_first(),
             'home_team_link': response.css("td.team:nth-of-type(1) a.team-link::attr(href)").extract_first(),
             'away_team_name': response.css("td.team:nth-of-type(3) a.team-link::text").extract_first(),
-            'away_team_name': response.css("td.team:nth-of-type(3) a.team-link::attr(href)").extract_first(),
+            'away_team_link': response.css("td.team:nth-of-type(3) a.team-link::attr(href)").extract_first(),
             'half_time': response.css("div.info-block:nth-of-type(2) dd:nth-of-type(1)::text").extract_first(),
             'full_time': response.css("div.info-block:nth-of-type(2) dd:nth-of-type(2)::text").extract_first(),
             'kick_off': response.css("div.info-block:nth-of-type(3) dd:nth-of-type(1)::text").extract_first(),
             'date': response.css("div.info-block:nth-of-type(3) dd:nth-of-type(2)::text").extract_first(),
-            'tournament_name': response.css("div div div div.layout-content-2col-left div div a::text").extract_first(),
-            'tournament_link': response.css("div div div div.layout-content-2col-left div div a::attr(href)").extract_first(),
+            'tournament_name': response.css("div#content-header a::text").extract_first(),
+            'tournament_link': response.css("div#content-header a::attr(href)").extract_first(),
             'country': response.css("span.iconize::text").extract_first()
         }
-                
+        
         
         #self.logger.info("Visited %s", response.url)        
         
         if len(match_report_link) > 10:            
             #return scrapy.Request(match_report_link, callback=self.parse_match_report)
-            yield SplashRequest(match_report_link, self.parse_match_report,
+            request = SplashRequest(match_report_link, 
+                callback=self.parse_match_report,
                 endpoint='render.html',
-                args={'wait': 0.5},
-                ) 
-        
+                args={'wait': 0.5}                
+                )
+            request.meta['match'] = match
+            yield request
+                    
     def parse_match_report(self, response):
-        yield {
-            'home_shots': response.css("div.stat-group.no-top-margin div.stat:nth-of-type(1) span.stat-value:nth-of-type(1)::text").extract_first(),
-            'home_possession': response.css("div.stat-group:nth-of-type(2) div.stat span.stat-value:nth-of-type(2)::text").extract_first(),
-            'home_shots': response.css("div.stat-group:nth-of-type(2) div.stat span.stat-value:nth-of-type(3)::text").extract_first()
-            }
+        
+        match = response.meta['match']
+        
+        match['home_shots'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(1) span.stat-value:nth-of-type(1) span::text").extract_first()
+        match['away_shots'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(1) span.stat-value:nth-of-type(3) span::text").extract_first()
+        match['home_shots_on_target'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(2) span.stat-value:nth-of-type(1) span::text").extract_first()
+        match['away_shots_on_target'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(2) span.stat-value:nth-of-type(3) span::text").extract_first()
+        match['home_pass_success'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(3) span.stat-value:nth-of-type(1) span::text").extract_first()
+        match['away_pass_success'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(3) span.stat-value:nth-of-type(3) span::text").extract_first()
+        match['home_aerial_duel_success'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(4) span.stat-value:nth-of-type(1) span::text").extract_first()
+        match['away_aerial_duel_success'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(4) span.stat-value:nth-of-type(3) span::text").extract_first()
+        match['home_dribbles_won'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(5) span.stat-value:nth-of-type(1) span::text").extract_first()
+        match['away_dribbles_won'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(5) span.stat-value:nth-of-type(3) span::text").extract_first()
+        match['home_tackles'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(6) span.stat-value:nth-of-type(1) span::text").extract_first()
+        match['away_tackles'] = response.css("div.stat-group.no-top-margin div.stat:nth-of-type(6) span.stat-value:nth-of-type(3) span::text").extract_first()
+        match['home_possession'] = response.css("div.stat-group:nth-of-type(2) div.stat span.stat-value:nth-of-type(2) span::text").extract_first()
+        match['away_possession'] = response.css("div.stat-group:nth-of-type(2) div.stat span.stat-value:nth-of-type(3) span::text").extract_first()
+        
+        return match
         
         
